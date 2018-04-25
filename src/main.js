a7f7fd2e7a25447d8e796f6803b4428682829585
dongzhou <xf_30el@126.com>
2018-04-25 11:27:12 +0800

    添加路由tree

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
+import store from './store/'
 import router from './router'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
@@ -10,6 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
